97da207fa79e8accb10dcd43e8553d76b2c371b9
teddy <70366832+teddy@users.noreply.github.com>
2020-12-12 00:18:29 +0800

    1212

---
+++ Easy/941. Valid Mountain Array/validMountainArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr == null || arr.length == 0){
+            return false;
+        }
+        int i = 0;
+        while(i + 1 < arr.length && arr[i] < arr[i+1]){
+            i++;
+        }
+        if(i == 0 || i == arr.length-1){
+            return false;
+        }
+        while(i + 1 < arr.length && arr[i] > arr[i+1]){
+            i++;
+        }
+        return i == arr.length-1;
+    }
+}
