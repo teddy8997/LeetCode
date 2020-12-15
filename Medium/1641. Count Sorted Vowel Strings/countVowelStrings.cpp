c6d3245cfbfc646abd56b3dcf61ddf81e65f72c2
teddy <70366832+teddy@users.noreply.github.com>
2020-12-15 21:14:06 +0800

    1215

--- Medium/1641. Count Sorted Vowel Strings/countVowelStrings.cpp
+++ Medium/1641. Count Sorted Vowel Strings/countVowelStrings.cpp
@@ -1,25 +1,18 @@
-/*
-    使用DP來解
-    dp[i][k]: 到第 i 個字符為止第i個字符是字母k的方案數
-    根據提議第i-1個字符必須小於等於k
-    最後我們要求的結果為 sum{dp[n-1][k]}, k = 0,1,2,3,4
+﻿/*
+    參考https://www.youtube.com/watch?v=gdt7HQF56OI&ab_channel=LeadCoding
     
 */
 class Solution {
 public:
     int countVowelStrings(int n) {
-        vector<vector<int>> dp(n+1, vector<int>(5));    
-        for(int k = 0; k < 5; k++){
-            dp[0][k] = 1;
-        }
-        
-        for(int i = 1; i < n; i++){
-            for(int k = 0; k < 5; k++){
-                for(int j = 0; j <= k; j++){
-                    dp[i][k] += dp[i-1][j];
-                }
+        vector<vector<int>> dp(n+1, vector<int>(5,1));     
+        for(int i = 2; i <= n; i++){
+            int s = 0;
+            for(int j = 0; j < 5; j++){
+                s += dp[i-1][j];
+                dp[i][j] = s;
             }
         }
-        return accumulate(dp[n-1].begin(), dp[n-1].end(),0);
+        return accumulate(dp[n].begin(), dp[n].end(),0);
     }
 };
