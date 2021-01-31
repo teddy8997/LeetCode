665e21e2ecb834368b75a04d756a04c3fe07b1e8
teddy <70366832+teddy@users.noreply.github.com>
2021-01-31 20:25:49 +0800

    210131

---
+++ Hard/1675. Minimize Deviation in Array/minimumDeviation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        priority_queue<int> q;
+        int low = INT_MAX;
+        for(int n : nums){
+            n = n % 2 == 0? n : n * 2;
+            q.push(n);
+            low = min(low, n);
+        }
+        int ans = q.top() - low;
+        while(q.top() % 2 == 0){
+            int x = q.top();
+            q.pop();
+            q.push(x / 2);
+            low = min(low, x / 2);
+            ans = min(ans, q.top() - low);
+        }
+        return ans;
+    }
+};
